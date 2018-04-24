cd3b191b29148553cf1ffe8fba189ec88c4401b1
Melissa Real <melissa.real@hotmail.com>
2018-04-24 14:41:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  new_arr = []
+  array.each do |values|
+    new_arr.push(values.split(";"))
+  end 
+  
+  len = new_arr.length
+ 
+  case len
+  
+  when 1
+   return new_arr.join(", ")
+  when 2 
+   return new_arr.join(" and ")
+  else 3 
+   return new_arr[0..-2].join(", ") + ", and " + new_arr[-1].join()
+  end
+  
+  
 end
